9099c83e1ca95266f3fbaf5c069e2b5d29493dd5
uvsq22001055 <elora.lefebvre@ens.uvsq.fr>
2020-10-20 13:46:14 +0200

    td3.py

--- td3.py
+++ td3.py
@@ -19,7 +19,7 @@ def secondeEnTemps(seconde):
    return temps
     
 temps = secondeEnTemps(100000)
-print(temps[0],"jours",temps[1],"heures",temps[2],"minutes",temps[3],"secondes")
+print(temps[0],"jours",temps[1],"heures",temps[2],"minutes",temps[3],"secondes\n")
 
 #fonction auxiliaire ici
 
@@ -53,7 +53,7 @@ def afficheTemps (temps):
     elif temps [3] == 0:
             print(end="")
     else:
-            print (temps[3], "secondes ", end="")
+            print (temps[3], "secondes\n ", end="")
     return afficheTemps
 
 afficheTemps((1,0,14,23))
@@ -91,45 +91,44 @@ def tempsEnDate(temps):
     h = ((s % 31536000) % 86400) // 3600
     mn = (((s % 31536000) % 86400) % 3600) // 60
     s = (((s % 31536000) % 86400) % 3600) % 60
-    temps =  (a, j, h, mn, s)
-    return temps
+    date =  (a, j, h, mn, s)
+    return date
 
+def afficheDate(date = -1):
+    temps_0 = (1970, 1, 00, 00, 00)
 
-def afficheDate(temps):
-    
-
-    if temps [1] == 1:
-            print (temps[1], "jour ", end="")
-    elif temps [1] == 0:
+    if date [1] == 1:
+            print (date[1] + 1,"ème jour de ", end="")
+    elif date [1] == 0:
             print(end="")
     else:
-            print (temps[1], "jours ", end="")
+            print (date[1] + 1,"ème jours de ", end="")
 
-    print(temps[0]," à ")
+    print(date[0] + 1970,"à ", end="")
 
-    if temps [2] == 1:
-            print (temps[2], "heure ", end="")
-    elif temps [2] == 0:
+    if date [2] == 1:
+            print (date[2], "heure ", end="")
+    elif date [2] == 0:
             print(end="")
     else: 
-            print (temps[2], "heures ", end="")
+            print (date[2], "heures ", end="")
 
-    if temps [3] == 1:
-            print (temps[3], "minute ", end="")
-    elif temps [3] == 0:
+    if date [3] == 1:
+            print (date[3], "minute ", end="")
+    elif date [3] == 0:
             print(end="")
     else:
-            print (temps[3], "minutes ", end="")
+            print (date[3], "minutes ", end="")
 
-    if temps [4] == 1:
-            print (temps[4], "seconde ", end="")
-    elif temps [4] == 0:
+    if date [4] == 1:
+            print (date[4], "seconde ", end="")
+    elif date [4] == 0:
             print(end="")
     else:
-            print (temps[4], "secondes ", end="")
+            print (date[4], "secondes\n ")
     return afficheDate
+
 
 temps = secondeEnTemps(1000000000)
 afficheTemps(temps)
-afficheDate(tempsEnDate(temps))
-afficheDate()
+afficheDate(tempsEnDate(temps))S
