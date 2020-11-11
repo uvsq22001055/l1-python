5f88956565753ccbbc151649e687b63d372eb8a7
uvsq22001055 <elora.lefebvre@ens.uvsq.fr>
2020-11-11 23:18:51 +0100

    syracuse.py

--- exercises/TD03_fonctions/syracuse.py
+++ exercises/TD03_fonctions/syracuse.py
@@ -1,1 +1,13 @@
-b
+def syracuse(n):
+    """ Retourne la liste des valeurs de la suite en partant de n jusqu'à 1 """
+    print(n)
+    while n != 1:
+        if n % 2 == 0:
+            n /= 2
+            print(n)
+        else:
+            n = (n * 3) + 1
+            print(n)
+
+
+print(syracuse(3))
