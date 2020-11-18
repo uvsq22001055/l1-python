1f4f30a8d3a5c9dbb1d73b14ba67d274c0348087
uvsq22001055 <elora.lefebvre@ens.uvsq.fr>
2020-11-18 11:58:17 +0100

    syracuse.py

--- exercises/TD04_listes/syracuse.py
+++ exercises/TD04_listes/syracuse.py
@@ -60,7 +60,8 @@ afficheCarre(carre_pas_mag)
 
 
 def testLignesEgales(carre):
-    """ Renvoie la somme des éléments d'une ligne de la liste 2D carre si toutes les lignes ont la même somme, et -1 sinon"""
+    """ Renvoie la somme des éléments d'une ligne de la liste 2D carre si\
+         toutes les lignes ont la même somme, et -1 sinon"""
     lenght = len(carre)
     list_somme = []
     for i in range(lenght):
@@ -78,12 +79,13 @@ print(testLignesEgales(carre_pas_mag))
 
 
 def testColonnesEgales(carre):
-    """ Renvoie la somme des éléments d'une colonne de la liste 2D carre si toutes les colonnes ont la même somme, et -1 sinon """
+    """ Renvoie la somme des éléments d'une colonne de la liste 2D carre si \
+        toutes les colonnes ont la même somme, et -1 sinon """
     nb_colonne = len(carre)
     list_somme = []
     for i in range(len(carre)):
         s = 0
-        for k in range(len(carre[i])): 
+        for k in range(len(carre[i])):
             s += carre[k][i]
         list_somme.append(s)
     if s * nb_colonne != sum(list_somme):
@@ -97,51 +99,50 @@ print(testColonnesEgales(carre_pas_mag))
 
 
 def testDiagonalesEgales(carre):
-    """ Renvoie la somme des éléments d'une diagonale de la liste 2D carre si les 2 diagonales ont la même somme, et -1 sinon """
+    """ Renvoie la somme des éléments d'une diagonale de la liste 2D carre si les\
+        2 diagonales ont la même somme, et -1 sinon """
     list_somme1 = []
     list_somme2 = []
     for i in range(len(carre)):
-        s = 0 
+        s = 0
         s += carre[i][i]
         list_somme1.append(s)
     for i in range(len(carre)):
-         s = 0 
-         s += carre[i][abs(i - (len(carre)) +1)]
-         list_somme2.append(s)
+        s = 0
+        s += carre[i][abs(i - (len(carre)) + 1)]
+        list_somme2.append(s)
 
-    if sum(list_somme2) != sum(list_somme1) :
+    if sum(list_somme2) != sum(list_somme1):
         return -1
     else:
         return sum(list_somme1)
 
+
 print(testDiagonalesEgales(carre_mag))
-print(testDiagonalesEgales(carre_pas_mag))S
+print(testDiagonalesEgales(carre_pas_mag))
 
 
 def estCarreMagique(carre):
-    """ Renvoie True si c'est un carre magique et False sinon"""
+    """ Renvoie True si c'est un carre magique et \
+        False sinon"""
     if testLignesEgales(carre) == testColonnesEgales(carre) == testDiagonalesEgales(carre):
         return True
-    else :
+    else:
         return False
+
 
 print(estCarreMagique(carre_mag))
 print(estCarreMagique(carre_pas_mag))
 
+
 def estNormal(carre):
-    """ Retourne True si contient toutes les valeurs de 1 à n^2 où n est la taille 
-        du carré, et False sinon """
-    list_sommeT = []
+    """ Retourne True si contient toutes les valeurs de \
+        1 à n^2 où n est la taille du carré, et False sinon """
     s = 0
     l_c = len(carre) ** 2
     for i in range(l_c):
         s += 1
-        list_sommeT.append(s)
-        
-    print(list_sommeT)
-    print(s)
-    print(l_c)
-    if list_sommeT == l_c:
+    if s == l_c:
         return True
     else:
         return False
