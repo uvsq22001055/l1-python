bc9de14548ec71a0bd577ff4f587e8ba8ec5dac1
uvsq22001055 <elora.lefebvre@ens.uvsq.fr>
2020-11-26 11:12:14 +0100

    dessin.py

--- exercises/TD05_gui/dessin.py
+++ exercises/TD05_gui/dessin.py
@@ -5,7 +5,7 @@ import tkinter as tk
 racine = tk.Tk()
 racine.title('Mon dessin')
 
-bouton = tk.Button(racine, text="choisir une couleur", font = ("helvetica", "10"), activebackground = '#BBBBBB')
+bouton = tk.Button(racine, text="choisir une couleur", font = ("helvetica", "10"), activebackground = '#BBBBBB', overrelief = 'groove')
 bouton1 = tk.Button(racine, text="cercle", font = ("helvetica", "10"), activebackground = '#D8D0AA')
 bouton2 = tk.Button(racine, text="carré", font = ("helvetica", "10"), activebackground = '#A0AACC')
 bouton3 = tk.Button(racine, text="croix", font = ("helvetica", "10"), activebackground = '#DFAAAA')
@@ -17,7 +17,7 @@ bouton3.grid(column = 0, row= 3)
 
 
 
-canvas = tk.Canvas(racine, bg='black', height=500, width=500)
+canvas = tk.Canvas(racine, bg='black', height=500, width=500, borderwidth = '10', relief = 'sunken')
 canvas.grid(column= 1, row = 1, rowspan = 3)
 
 racine.mainloop() # Lancement de la boucle principale
