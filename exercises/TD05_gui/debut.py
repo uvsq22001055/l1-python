da2a5ed30ac969b4889c488471584f8da4608e12
uvsq22001055 <elora.lefebvre@ens.uvsq.fr>
2020-11-26 09:37:56 +0100

    debut.py

--- exercises/TD05_gui/debut.py
+++ exercises/TD05_gui/debut.py
@@ -11,7 +11,7 @@ canvas = tk.Canvas(root, width = CANVAS_WIDTH, height = CANVAS_HEIGHT)
 x0 = 100 #ligne imaginaire vertical en x = 100unité depuis le bord gauche(centre du premier cercle a gauche)
 x1 = CANVAS_WIDTH - 100 #ligne imaginaire vertical en x = -100unité depuis le bord droit(centre du premier cercle a droite)
 y = CANVAS_HEIGHT / 2 #hauteur du centre de la feuille
-canvas.create_line(x0, y, x1, y)  #ligne du point 1(x0, y) au point 2(x1, y)
+canvas.create_line(CANVAS_WIDTH/2, CANVAS_HEIGHT, CANVAS_WIDTH/2, - CANVAS_HEIGHT)  #ligne du point 1(x0, y) au point 2(x1, y)
 canvas.create_oval(x0 - 50, y + 50, x0 + 50, y - 50) #creation ovale -> cerlce, de diametre 100 (cercle de gauche) de centre x0
 canvas.create_oval(x1 - 50, y + 50, x1 + 50, y - 50) #creation ovale -> cerlce, de diametre 100 (cercle de droite)de centre x1
 canvas.create_oval((x0 + x1) / 2 - 50, y + 50, (x0 + x1) / 2 + 50, y - 50) #creation ovale -> cerlce, de diametre 100 (cercle du mileu) centre milieu[x0, x1]
