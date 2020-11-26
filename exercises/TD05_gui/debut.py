d29647e1de168a602870e3c0014e896e08b1ba11
uvsq22001055 <elora.lefebvre@ens.uvsq.fr>
2020-11-26 09:35:59 +0100

    debut.py

--- exercises/TD05_gui/debut.py
+++ exercises/TD05_gui/debut.py
@@ -1,11 +1,11 @@
-from tkinter import *
+import tkinter as tk
 
 CANVAS_WIDTH, CANVAS_HEIGHT = 600, 400
 
 
-root = Tk()
+root = tk.Tk()
 
-canvas = Canvas(root, width = CANVAS_WIDTH, height = CANVAS_HEIGHT)
+canvas = tk.Canvas(root, width = CANVAS_WIDTH, height = CANVAS_HEIGHT)
 
 # Début de votre code
 x0 = 100 #ligne imaginaire vertical en x = 100unité depuis le bord gauche(centre du premier cercle a gauche)
@@ -15,8 +15,7 @@ canvas.create_line(x0, y, x1, y)  #ligne du point 1(x0, y) au point 2(x1, y)
 canvas.create_oval(x0 - 50, y + 50, x0 + 50, y - 50) #creation ovale -> cerlce, de diametre 100 (cercle de gauche) de centre x0
 canvas.create_oval(x1 - 50, y + 50, x1 + 50, y - 50) #creation ovale -> cerlce, de diametre 100 (cercle de droite)de centre x1
 canvas.create_oval((x0 + x1) / 2 - 50, y + 50, (x0 + x1) / 2 + 50, y - 50) #creation ovale -> cerlce, de diametre 100 (cercle du mileu) centre milieu[x0, x1]
-    
+canvas.grid()
 # Fin de votre code
 
-canvas.pack()
 root.mainloop()
