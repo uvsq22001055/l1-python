a1d1219d08a923223d7f7c981b4345ed2e6a6d7f
uvsq22001055 <elora.lefebvre@ens.uvsq.fr>
2020-12-02 16:55:11 +0100

    couleur.py

--- exercises/TD05_gui/couleur.py
+++ exercises/TD05_gui/couleur.py
@@ -1,10 +1,11 @@
 import tkinter as tk
+import random as rd
 
 CANVAS_WIDTH, CANVAS_HEIGHT = 256, 256
 
 racine = tk.Tk()
 
-canvas = tk.Canvas(racine, bg='black', height=CANVAS_HEIGHT, width=CANVAS_HEIGHT, borderwidth = '10', relief = 'sunken')
+canvas = tk.Canvas(racine, bg='black', height=CANVAS_HEIGHT, width=CANVAS_HEIGHT)
 
 
 def get_color(r, g, b):
@@ -12,12 +13,22 @@ def get_color(r, g, b):
     return '#{:02x}{:02x}{:02x}'.format(r, g, b)
 
 def draw_pixel(i, j, color):
-    pixel = canvas.create_rectangle(i, i, j, j, fill = color, width=0)
+    pixel = canvas.create_rectangle(i, j, i, j, fill = color, width=0)
     return pixel
 
-draw_pixel(128, 129, 'white')
 
-bouton1 = tk.Button(racine, text="Aléatoire", font = ("Courier", "20"), activebackground = '#BBBBBB', foreground='blue')
+def ecran_aleatoire():
+    for i in range(0, 258):
+        for j in range(0, 258):
+            list_color = ['white', 'black', 'red', 'green', 'blue', 'cyan', 'yellow', 'magenta']
+            color = rd.choice(list_color)
+            draw_pixel(i, j, color)
+
+
+
+
+
+bouton1 = tk.Button(racine, text="Aléatoire", font = ("Courier", "20"), command = ecran_aleatoire , activebackground = '#BBBBBB', foreground='blue')
 bouton2 = tk.Button(racine, text="Dégradé gris", font = ("Courier", "20"), activebackground = '#BBBBBB',foreground='blue')
 bouton3 = tk.Button(racine, text="Dégradé 2D", font = ("Courier", "20"), activebackground = '#BBBBBB', foreground='blue')
 
